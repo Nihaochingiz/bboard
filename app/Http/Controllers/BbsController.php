c865cdbe46aed9871d9cee9797cf73dddf494d79
gaijin <ilyaf1999@gmail.com>
2026-01-30 17:37:16 +0300

    feat: add finding records by id

--- app/Http/Controllers/BbsController.php
+++ app/Http/Controllers/BbsController.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Bb;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class BbsController extends Controller
 {
@@ -11,9 +13,27 @@ class BbsController extends Controller
         $s = "Объявления \r\n\r\n";
         foreach ($bbs as $bb) {
             $s .= $bb->title . "\r\n";
-            $s .=$bb->price . " руб.\r\n";
+            $s .= $bb->price . " руб.\r\n";
             $s .= "\r\n";
         }
-        return response($s)->header('Content-Type', 'text/plain');
+        return response($s)
+            ->header('Content-Type', 'text/plain; charset=utf-8');
+    }
+
+    public function detail($id) {
+        $bb = Bb::find($id);
+        
+        if (!$bb) {
+            return response("Объявление не найдено\r\n")
+                ->header('Content-Type', 'text/plain; charset=utf-8')
+                ->setStatusCode(404);
+        }
+        
+        $s = $bb->title . "\r\n\r\n";
+        $s .= $bb->content . "\r\n";
+        $s .= $bb->price . " руб.\r\n";
+        
+        return response($s)
+            ->header('Content-Type', 'text/plain; charset=utf-8');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BbsController;
 
 Route::get('/', [BbsController::class, 'index']);
+Route::get('/{bb}', [BbsController::class, 'detail']);
 
